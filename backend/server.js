9a4797111f789589bae814c431170476632fed38
deyvido <deyvidfr@hotmail.com>
2022-07-29 19:36:45 -0400

    20 Create Model and Routes Product

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -16,9 +18,18 @@ mongoose
 
 const app = express();
 
+// Routes
+app.use('/api/seed', seedRouter);
+
+//test
+/*
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+*/
+app.use('/api/products', productRouter);
+/* 
+//this ones go to productRoutes.js
 
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
@@ -36,6 +47,7 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
+*/
 
 const port = process.env.PORT || 5001; // no funciona con port 5000, dice que est'a en uso
 app.listen(port, () => {
