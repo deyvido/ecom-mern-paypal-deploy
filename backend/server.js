0085a4db032d87ed7b2239c12c47982113a608d8
deyvido <deyvidfr@hotmail.com>
2022-08-03 12:11:29 -0400

    33-publish-heroky

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -60,6 +61,13 @@ app.get('/api/products/:id', (req, res) => {
 
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
